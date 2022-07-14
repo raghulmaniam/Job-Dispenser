2d71bebc3dda4a4180695e325b0caa6c9a619119
alap <120220216@umail.ucc.ie>
2022-07-14 01:49:12 +0100

    initial project structure commited

--- src/main/java/com/raghul/workorderpriorityqueue/WorkOrderPriorityQueueApplication.java
+++ src/main/java/com/raghul/workorderpriorityqueue/WorkOrderPriorityQueueApplication.java
@@ -8,6 +8,8 @@ public class WorkOrderPriorityQueueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkOrderPriorityQueueApplication.class, args);
+		
+		
 	}
 
 }
